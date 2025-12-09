882cf03c4d6186b8a546e2fae39b3bddbb572321
JulianAngarita <angaritaarias632@gmail.com>
2025-12-09 11:47:20 -0500

    ajuste prod 4

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://backend-plus.onrender.com/',
+  api: 'https://backend-plus.onrender.com',
   frontend: 'https://clienteweb-mkplus.netlify.app/',
   bucket: 'ivuo-dev',
   paths: {
